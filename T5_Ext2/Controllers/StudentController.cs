7385164f1efd3d6139182901acb521f27b341f2d
KienLy_KOI <12200143@students.koi.edu.au>
2022-12-12 00:50:38 +1100

    upload a project name T5_Ext2
    upload all file created in project T5_Ext2

---
+++ T5_Ext2/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace T5_Ext2.Controllers
+{
+    public class StudentController: Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Subjects()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ T5_Ext2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace T5_Ext2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
